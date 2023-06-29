d7611c9f68c25aafe7e78d5bb175ff7f3b111dde
Tyler Miller <tyler.e.miller1995@gmail.com>
2023-06-28 21:23:25 -0500

    feat: Add AdminAuthRequirement for authorization
    This commit adds a new file `AdminAuthRequirement.cs` which implements the `IAuthorizationHandler` and `IAuthorizationRequirement` interfaces. The `AdminAuthRequirement` class handles authorization logic for admin users. It checks if the user has the admin claim, and if not, it verifies the API key provided in the request headers. If the API key is valid, it adds a custom claim to the user's identity.
    
    Additionally, in `Program.cs`, an instance of `AdminAuthRequirement` is added to the policy requirements for the `AdminUserPolicyName`. This ensures that only users with admin privileges or a valid API key can access restricted resources.
    
    Note: The previous line adding a policy requirement using claims has been commented out.

---
+++ Movies.Api/Auth/AdminAuthRequirement.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.Api.Auth;
+
+public class AdminAuthRequirement : IAuthorizationHandler, IAuthorizationRequirement
+{
+    private readonly string _apiKey;
+
+    public AdminAuthRequirement(string apiKey)
+    {
+        _apiKey = apiKey;
+    }
+
+    public Task HandleAsync(AuthorizationHandlerContext context)
+    {
+        if (context.User.HasClaim(AuthConstants.AdminUserClaimName, "true"))
+        {
+            context.Succeed(this);
+            return Task.CompletedTask;
+        }
+
+        var httpContext = context.Resource as HttpContext;
+        if (httpContext is null)
+        {
+            return Task.CompletedTask;
+        }
+        
+        if (!httpContext.Request.Headers.TryGetValue(AuthConstants.ApiKeyHeaderName, out var extractedApiKey))
+        {
+            context.Fail();
+            return Task.CompletedTask;
+        }
+        
+        if (_apiKey != extractedApiKey)
+        {
+            context.Fail();
+            return Task.CompletedTask;
+        }
+
+        var identity = (ClaimsIdentity)httpContext.User.Identity!;
+        identity.AddClaim(new Claim("userid", Guid.Parse("bc7f9d35-b789-4f49-8174-786a58d5e61f").ToString()));
+        context.Succeed(this);
+        return Task.CompletedTask;
+    }
+}
